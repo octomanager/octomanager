0891ec94fbea6c99d4566fe24005da2bb6c3edf3
Daniel Watkins <daniel.watkins@hogarthww.com>
2013-10-25 11:56:24 +0100

    Refactor octomanage to use a class.

--- octomanager.py
+++ octomanager.py
@@ -6,16 +6,26 @@ def _get_authd_github():
     return Github("63c645c4c54933d1364e3f5367a55defe24b626f")
 
 
+class OctoManage(object):
+
+    def __init__(self):
+        self.github = _get_authd_github()
+        self._perform_pull_request_assignment('octomanager/test-repo')
+
+    def _get_assignee(self):
+        return self.github.get_user('arachnegl')
+
+    def _perform_pull_request_assignment(self, repo_name):
+        repo = self.github.get_repo(repo_name)
+        pull_requests = repo.get_pulls()
+        for pull_request in pull_requests:
+            if pull_request.assignee is None:
+                issue = repo.get_issue(pull_request.number)
+                issue.edit(assignee=self._get_assignee())
+
+
 def octomanage():
-    # poll github for open pull requests
-    # set assignee for any unassigned pull requests
-    github = _get_authd_github()
-    repo = github.get_repo('octomanager/test-repo')
-    pull_requests = repo.get_pulls()
-    for pull_request in pull_requests:
-        if pull_request.assignee is None:
-            issue = repo.get_issue(pull_request.number)
-            issue.edit(assignee=github.get_user('arachnegl'))
+    OctoManage()
 
 
 if __name__ == '__main__':
